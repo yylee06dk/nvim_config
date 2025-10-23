40bd4035585bb50d53c42587dbf2d65cb68b7838
yylee <yylee06dk@gmail.com>
2025-10-24 03:33:44 +0900

    Disable flashs.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,4 +1,4 @@
 return {
   "folke/flash.nvim",
-  opts = { enabled = false }, -- disable flash. (To use original "s" cmd in vim)
+  enabled = false, -- disable flash. (To use original "s" cmd in vim)
 }
