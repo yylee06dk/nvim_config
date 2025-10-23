51ecba9bc185888238eb2948cec59d7f06770bfe
yylee <yylee06dk@gmail.com>
2025-10-24 03:31:31 +0900

    Update snacks.nvim about indent and notifier

--- lua/plugins/snack.lua
+++ lua/plugins/snack.lua
@@ -1,1 +1,16 @@
-return {}
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      indent = { -- disable animated(dynamic) indent guides
+        enabled = true,
+        animate = { enabled = false },
+        scope = { enabled = false },
+      },
+      notifier = { -- bit longer notifications
+        enabled = true,
+        timeout = 5000,
+      },
+    },
+  },
+}
